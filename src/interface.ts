4d2584f6569542cfc5f4b8b6b8d83c2fc4aee023
unadlib <unadlib@gmail.com>
2020-09-24 01:38:25 +0800

    feat(originstorage): implement OriginStorage

--- src/interface.ts
+++ src/interface.ts
@@ -1,18 +1,32 @@
 import {
   IFrameMainTransportOptions,
   IFrameTransportInternalOptions,
+  TransportData,
 } from 'data-transport';
 import localforage from 'localforage';
-import { databases } from './constant';
+// import { databases } from './constant';
 
-type LocalForageOptions = Parameters<typeof localforage.createInstance>;
+type LocalForageOptions = Parameters<typeof localforage.createInstance>[0];
 
 export interface OriginStorageClientOptions extends IFrameMainTransportOptions {
-  uri: string;
-  type?: keyof typeof databases;
+  // type?: keyof typeof databases;
 }
 
 export interface OriginStorageOptions extends IFrameTransportInternalOptions {
   read?: boolean;
   write?: boolean;
 }
+
+export type StorageToClient = {
+  connect: TransportData<void, LocalForageOptions>;
+};
+
+export type ClientToStorage = {
+  getItem: TransportData<{ key: string }, { value: any }>;
+  setItem: TransportData<{ key: string; value: any }, void>;
+  removeItem: TransportData<{ key: string }, void>;
+  clear: TransportData<void, void>;
+  length: TransportData<void, { length: number }>;
+  key: TransportData<{ index: number }, { key: string }>;
+  keys: TransportData<void, { keys: string[] }>;
+};
--- src/originStorage.ts
+++ src/originStorage.ts
@@ -1,11 +1,81 @@
+import { IFrameTransport, Receiver, listen, Listen } from 'data-transport';
 import localforage from 'localforage';
+import {
+  ClientToStorage,
+  OriginStorageOptions,
+  StorageToClient,
+} from './interface';
 
-export class OriginStorage {
-  protected _localforage: ReturnType<typeof localforage.createInstance>;
+export class OriginStorage
+  extends IFrameTransport.IFrame<StorageToClient>
+  implements Receiver<ClientToStorage> {
+  protected _localforage!: ReturnType<typeof localforage.createInstance>;
+  protected _read: boolean;
+  protected _write: boolean;
 
-  constructor() {
-    this._localforage = localforage.createInstance({
-      //
-    });
+  constructor({ read = true, write = true, ...options }: OriginStorageOptions) {
+    super(options);
+    this._read = read;
+    this._write = write;
+  }
+
+  async connect() {
+    if (!this._read && !this._write) {
+      throw new Error('The OriginStorage does not have any read/write access.');
+    }
+    const config = await this.emit('connect', undefined);
+    this._localforage = localforage.createInstance(config);
+  }
+
+  @listen
+  async getItem({ request, respond }: Listen<ClientToStorage['getItem']>) {
+    if (!this._read) return;
+    const value = await this._localforage.getItem(request.key);
+    respond({ value });
+  }
+
+  @listen
+  async setItem({ request, respond }: Listen<ClientToStorage['setItem']>) {
+    if (!this._write) return;
+    await this._localforage.setItem(request.key, request.value);
+    respond();
+  }
+
+  @listen
+  async removeItem({
+    request,
+    respond,
+  }: Listen<ClientToStorage['removeItem']>) {
+    if (!this._write) return;
+    await this._localforage.removeItem(request.key);
+    respond();
+  }
+
+  @listen
+  async clear({ respond }: Listen<ClientToStorage['clear']>) {
+    if (!this._write) return;
+    await this._localforage.clear();
+    respond();
+  }
+
+  @listen
+  async length({ respond }: Listen<ClientToStorage['length']>) {
+    if (!this._read) return;
+    const length = await this._localforage.length();
+    respond({ length });
+  }
+
+  @listen
+  async key({ request, respond }: Listen<ClientToStorage['key']>) {
+    if (!this._read) return;
+    const key = await this._localforage.key(request.index);
+    respond({ key });
+  }
+
+  @listen
+  async keys({ respond }: Listen<ClientToStorage['keys']>) {
+    if (!this._read) return;
+    const keys = await this._localforage.keys();
+    respond({ keys });
   }
 }
