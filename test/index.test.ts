64e3c09eea616f2bcb07a456cbe997e9a68d3ec2
unadlib <unadlib@gmail.com>
2020-10-06 15:39:27 +0800

    test(test): add unit test

--- test/index.test.ts
+++ test/index.test.ts
@@ -32,7 +32,11 @@ class MemoryStorage {
   }
 }
 
-const getOriginStorageClient = async () => {
+const getInstances = async ({
+  read = true,
+  write = true,
+  broadcastChanges = false,
+} = {}) => {
   let mockExternalSend: (...args: any) => void;
   let mockInternalSend: (...args: any) => void;
 
@@ -51,6 +55,9 @@ const getOriginStorageClient = async () => {
   originStorageClient.onConnect(fn);
 
   const originStorage = new OriginStorage({
+    read,
+    write,
+    broadcastChanges,
     listener: (callback) => {
       mockExternalSend = callback;
     },
@@ -60,14 +67,17 @@ const getOriginStorageClient = async () => {
   });
 
   expect(fn.mock.calls.length).toBe(1);
-  await new Promise(r => setTimeout(r));
+  await new Promise((r) => setTimeout(r));
   // mock
   (originStorage as any)._localforage = new MemoryStorage();
-  return originStorageClient;
-}
+  return {
+    originStorageClient,
+    originStorage,
+  };
+};
 
 test('base', async () => {
-  const originStorageClient = await getOriginStorageClient();
+  const { originStorageClient } = await getInstances();
   expect(await originStorageClient.getItem('v')).toBeNull();
 
   const value = { a: 1 };
@@ -97,4 +107,162 @@ test('base', async () => {
 
   await originStorageClient.clear();
   expect(await originStorageClient.keys()).toEqual([]);
+});
+
+test('only write', async () => {
+  const { originStorageClient } = await getInstances({
+    read: false,
+    write: true,
+  });
+  const value = { a: 1 };
+  await originStorageClient.setItem('v', value);
+  const timeoutError = new Error('timeout');
+  let result = await Promise.race([
+    originStorageClient.getItem('v'),
+    new Promise((r) =>
+      setTimeout(() => {
+        r(timeoutError);
+      }, 100)
+    ),
+  ]);
+  expect(result === timeoutError).toBeTruthy();
+
+  result = await Promise.race([
+    originStorageClient.keys(),
+    new Promise((r) =>
+      setTimeout(() => {
+        r(timeoutError);
+      }, 100)
+    ),
+  ]);
+  expect(result === timeoutError).toBeTruthy();
+
+  result = await Promise.race([
+    originStorageClient.length(),
+    new Promise((r) =>
+      setTimeout(() => {
+        r(timeoutError);
+      }, 100)
+    ),
+  ]);
+  expect(result === timeoutError).toBeTruthy();
+
+  result = await Promise.race([
+    originStorageClient.key(0),
+    new Promise((r) =>
+      setTimeout(() => {
+        r(timeoutError);
+      }, 100)
+    ),
+  ]);
+  expect(result === timeoutError).toBeTruthy();
+});
+
+test('only read', async () => {
+  const { originStorageClient } = await getInstances({
+    read: true,
+    write: false,
+  });
+  const value = { a: 1 };
+  const timeoutError = new Error('timeout');
+  let result = await Promise.race([
+    originStorageClient.setItem('v', value),
+    new Promise((r) =>
+      setTimeout(() => {
+        r(timeoutError);
+      }, 100)
+    ),
+  ]);
+  expect(result === timeoutError).toBeTruthy();
+
+  result = await Promise.race([
+    originStorageClient.removeItem('v'),
+    new Promise((r) =>
+      setTimeout(() => {
+        r(timeoutError);
+      }, 100)
+    ),
+  ]);
+  expect(result === timeoutError).toBeTruthy();
+
+  result = await Promise.race([
+    originStorageClient.clear(),
+    new Promise((r) =>
+      setTimeout(() => {
+        r(timeoutError);
+      }, 100)
+    ),
+  ]);
+  expect(result === timeoutError).toBeTruthy();
+
+  expect(await originStorageClient.getItem('v')).toBeNull();
+});
+
+test('watch data change', async () => {
+  const instances0 = await getInstances({
+    broadcastChanges: true,
+  });
+  const instances1 = await getInstances({
+    broadcastChanges: true,
+  });
+
+  (instances0.originStorage as any)._broadcastChannel = {
+    postMessage: (message: any) =>
+      (instances1.originStorage as any)._broadcastChannel.onmessage(message),
+    onmessage: (message: any) => {
+      (instances0.originStorage as any).emit('change', message, {
+        respond: false,
+      });
+    },
+  };
+  (instances1.originStorage as any)._broadcastChannel = {
+    postMessage: (message: any) =>
+      (instances0.originStorage as any)._broadcastChannel.onmessage(message),
+
+    onmessage: (message: any) => {
+      (instances1.originStorage as any).emit('change', message, {
+        respond: false,
+      });
+    },
+  };
+  (instances1.originStorage as any)._localforage = (instances0.originStorage as any)._localforage;
+
+  const value = { a: 1 };
+  let watch = new Promise((r) => {
+    instances1.originStorageClient.onChange((data) => {
+      expect(data).toEqual({
+        key: 'v',
+        value,
+      });
+      r();
+    });
+  })
+  await instances0.originStorageClient.setItem('v', value);
+  await watch;
+
+  watch = new Promise((r) => {
+    instances1.originStorageClient.onChange((data) => {
+      expect(data).toEqual({
+        key: 'v',
+        value: null,
+      });
+      r();
+    });
+  })
+  await instances0.originStorageClient.removeItem('v');
+  await watch;
+
+  await instances1.originStorageClient.onChange(() => {});
+  await instances0.originStorageClient.setItem('x', 1);
+  await instances0.originStorageClient.setItem('y', 1);
+  watch = new Promise((r) => {
+    instances1.originStorageClient.onChange((data) => {
+      expect(data).toEqual({
+        key: null,
+      });
+      r();
+    });
+  })
+  instances0.originStorageClient.clear();
+  await watch;
 });
